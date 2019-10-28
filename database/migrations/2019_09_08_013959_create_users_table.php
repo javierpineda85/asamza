1469f19a67c576d6211303b0ed79497060eb22db
Javier <javierpineda85@gmail.com>
2019-10-28 18:38:30 -0300

    actualizacion de migraciones de usuarios

--- database/migrations/2019_09_08_013959_create_users_table.php
+++ database/migrations/2019_09_08_013959_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('lastname');
             $table->string('email');
+            $table->int('phone');
+            $table->int('level');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
