f32720dfbdcaf1eb64dac0388bfa41ae7b6075b1
Javier <javierpineda85@gmail.com>
2019-10-28 18:35:52 -0300

    Actualizacion de modelos

--- app/Tramite.php
+++ app/Tramite.php
@@ -2,10 +2,11 @@
 namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Muni;
+use App\Tramite;
 class Tramite extends Model
 {
   protected $fillable = ['title', 'description', 'file','muni_id'];
-  
+
   public $guarded=[];
       public function muni() {
         return $this->belongsTo("App\Muni", "muni_id");
@@ -13,4 +14,7 @@ class Tramite extends Model
       public function tramite() {
         return $this->belongsTo("App\Tramite", "tramite_id");
       }
+
+      //Query Scope
+
 }
--- app/User.php
+++ app/User.php
@@ -35,4 +35,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Query Scope
+
+    public function scopeName($query, $name){
+      if($name)
+        return $query->where('name', 'LIKE', "%$name%");
+
+    }
+
+    public function scopeEmail($query, $email){
+      if($email)
+        return $query->where('email', 'LIKE', "%$email%");
+
+    }
+
+    public function scopeLastName($query, $lastName){
+      if($lastName)
+        return $query->where('lastName', 'LIKE', "%$lastName%");
+
+    }
 }
